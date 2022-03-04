0546e59d4994c322a4f856ed5362ca7edc49830b
KorobeiJT <jtdemulatier@gmail.com>
2022-03-04 08:47:55 -0500

    print grid

---
+++ createSudoku.py
@@ -0,0 +1,25 @@
+import turtle
+from random import randint, shuffle
+from time import sleep
+
+grid = []
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+def printGrid():
+    for i in range(9):
+        for j in range(9):
+            print( str(grid[i][j]) + " ", end="")
+            if (j%3==2 and j!=8):
+                print("| ", end="")
+        print()
+        if (i%3==2 and i!=8):
+            print("- - - - - - - - - - -")
+printGrid()
